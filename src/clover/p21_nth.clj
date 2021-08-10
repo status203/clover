d6c45bc93802cb8e9d8785e68b9d01b99bee2d8e
status203 <git@status203.com>
2021-08-10 13:38:33 +0100

    P22 version of nth using loop/recur

--- src/clover/p21_nth.clj
+++ src/clover/p21_nth.clj
@@ -1,16 +1,23 @@
 (ns clover.p21-nth)
 
-(defn my-nth [s index]
+(defn zipmap-nth [s index]
   ((-> index inc range (zipmap s))
    index))
 
+(defn reduce-nth [s index]
+  (loop [s s index index]
+    (if (zero? index) (first s)
+        (recur (rest s) (dec index)))))
+
+(defn my-nth [s index]
+  (reduce-nth s index))
+
 (comment
-  (my-nth '(4 5 6 7) 2)
   (= (my-nth '(4 5 6 7) 2) 6)
 
   (= (my-nth [:a :b :c] 0) :a)
 
   (= (my-nth [1 2 3 4] 1) 2)
-
+  
   (= (my-nth '([1 2] [3 4] [5 6]) 2) [5 6])
   )
