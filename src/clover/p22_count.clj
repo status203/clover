8ddb8dda905f466b60a278f9fe560231150bdbde
status203 <git@status203.com>
2021-08-10 13:50:20 +0100

    P22 version of count using reduce

---
+++ src/clover/p22_count.clj
@@ -0,0 +1,17 @@
+(ns clover.p22-count)
+
+(defn my-count
+  [s]
+  (reduce (fn [acc _] (inc acc)) 0 s))
+
+(comment
+  (= (my-count '(1 2 3 3 1)) 5)
+
+  (= (my-count "Hello World") 11)
+
+  (= (my-count [[1 2] [3 4] [5 6]]) 3)
+
+  (= (my-count '(13)) 1)
+
+  (= (my-count '(:a :b :c)) 3)g
+  )
